e8aac180de96074f2d9bcc203d1cd2c46f30d113
doinel1a <doinel1atanasiu@gmail.com>
2024-03-19 20:38:05 +0100

    chore: install & configure pino as custom logger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import env from './lib/env';
 import express from 'express';
+import log from './lib/logger';
 
 const PORT = env.PORT || 9876;
 
 const server = express();
 
 server.listen(PORT, () => {
-	console.log(`SERVER LISTENING ON: http://localhost:${PORT}`);
+	log.info(`SERVER LISTENING ON: http://localhost:${PORT}`);
 });
